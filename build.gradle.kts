74632fd60b797ae426104a35a54ee52389cf0a49
sobhan <sobhan.aryannamazi@gmail.com>
2024-07-08 15:40:09 +0330

    Added build-logic module and convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,10 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 subprojects {
